9da13e7462b17f96c0d76625704ee5e05a524e78
SteelPh0enix <wojciech_olech@hotmail.com>
2025-08-12 22:38:14 +0200

    Implemented proper python version detection in uv.rs

--- src/external_tools/uv.rs
+++ src/external_tools/uv.rs
@@ -1,26 +1,79 @@
-use std::{path::PathBuf, process::Command};
-
 use crate::external_tools::ExternalTool;
+use crate::external_tools::version::Version;
+
+use std::path::PathBuf;
+use std::process::Command;
+use std::str::FromStr;
+
 use which;
 
 pub struct Uv {
     path: PathBuf,
 }
 
+#[derive(Debug, PartialEq, Clone)]
 pub struct PythonInstance {
-    version: String,
-    path: PathBuf,
+    pub id: String,
+    pub version: Version,
+    pub path: Option<PathBuf>,
+}
+
+impl FromStr for PythonInstance {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Some((id, path_info)) = s.split_once(" ") {
+            let version: Version = id.parse().map_err(|_| ())?;
+            let path = if path_info.contains("<download available>") {
+                None
+            } else {
+                if path_info.contains(" -> ") {
+                    if let Some((first_path, _)) = path_info.split_once(" -> ") {
+                        // unwrap is infallible here
+                        Some(PathBuf::from_str(first_path.trim()).unwrap())
+                    } else {
+                        None
+                    }
+                } else {
+                    // unwrap is infallible here
+                    Some(PathBuf::from_str(path_info.trim()).unwrap())
+                }
+            };
+
+            Ok(PythonInstance {
+                id: String::from(id),
+                version,
+                path,
+            })
+        } else {
+            Err(())
+        }
+    }
 }
 
 impl Uv {
-    pub fn list_python_instances(&self) -> Result<Vec<PythonInstance>, std::io::Error> {
+    pub fn get_python_instances(&self) -> Result<Vec<PythonInstance>, std::io::Error> {
         let output = Command::new(&self.path)
             .arg("python")
             .arg("list")
             .output()?;
 
-        println!("{:?}", output);
-        Ok(vec![])
+        if !output.status.success() {
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("'uv python list' failed: {}", stderr),
+            ));
+        }
+
+        let stdout = String::from_utf8_lossy(&output.stdout);
+
+        let instances = stdout
+            .lines()
+            .filter_map(|line| line.parse().ok())
+            .collect();
+
+        Ok(instances)
     }
 }
 
@@ -56,6 +109,12 @@ mod tests {
     #[test]
     fn test_list_python_instances() {
         let uv = Uv::global().unwrap();
-        let python_instances = uv.list_python_instances();
+        let python_instances = uv.get_python_instances();
+        assert!(python_instances.is_ok());
+        let instances = python_instances.unwrap();
+        assert!(!instances.is_empty());
+        for instance in instances {
+            println!("{:?}", instance);
+        }
     }
 }
